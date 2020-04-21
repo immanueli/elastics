c61bd19d2f979107073d3497f1c10fe7a4c9cc49
lovehaolive@gmail.com <hao19930205>
2020-04-21 17:27:07 +0800

    1：后台系统搭建 2: 测试jpa连接数据库 3：Junit测试

---
+++ src/main/java/com/imma/elastics/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.imma.elastics.repository;
+
+import com.imma.elastics.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+
+}

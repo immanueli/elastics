4dd41e15969010681b4f80e3ca6f3c2659ed9508
lovehaolive@gmail.com <hao19930205>
2020-04-27 10:39:12 +0800

    1:设置跳转页面

--- src/main/java/com/imma/elastics/controller/HomeController.java
+++ src/main/java/com/imma/elastics/controller/HomeController.java
@@ -1,23 +1,53 @@
 package com.imma.elastics.controller;
 
-import com.imma.elastics.base.ApiResponse;
-import org.springframework.ui.Model;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+@Controller
 public class HomeController {
-    @GetMapping("/hello")
-    public ModelAndView hello(Model model){
-        ModelAndView index = new ModelAndView("index");
-        model.addAttribute("name","站长");
-        return index;
+    /**
+     * 首页
+     * @return
+     */
+    @GetMapping("/index")
+    public String index(){
+        return "index";
     }
 
-    @GetMapping("/getMessage")
-    public ApiResponse getMessage(){
-        return ApiResponse.ofMessage(200,"请求成功");
+    /**
+     * 404页面
+     * @return
+     */
+    @GetMapping("/404")
+    public String notFoundPage(){
+        return "404";
     }
+
+    /**
+     * 500页面
+     * @return
+     */
+    @GetMapping("/500")
+    public String internalError(){
+        return "500";
+    }
+
+    /**
+     * 403页面
+     * @return
+     */
+    @GetMapping("/403")
+    public String accessError(){
+        return "403";
+    }
+
+    /**
+     * logout页面
+     */
+    @GetMapping("/logout")
+    public String logout(){
+        return "logout";
+    }
+
 
 }
